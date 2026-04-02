0999cf2d15ffabf1347d9b401a7ceac43ce58141
Fernando Maya de Morais <fernandomayademorais@gmail.com>
2026-04-01 22:06:43 -0300

    Update projeto.asm

--- semana02/projeto.asm
+++ semana02/projeto.asm
@@ -1,1 +1,18 @@
+       	INP
+	      STA X
+	      INP
+	      STA Y
+	      INP
+	      STA Z
+	      LDA X
+	      ADD Y
+	      STA X
+	      LDA X
+	      SUB Z
+	      STA X
+	      OUT
+	      HLT
 
+X	      DAT
+Y	      DAT
+Z	      DAT  
